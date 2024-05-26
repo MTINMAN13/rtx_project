ef970cfb5827dd8bbfa0df18fdb6695cebf12d4c
mman <martinjman27@gmail.com>
2024-05-26 14:18:15 +0200

    add parser func

--- parser.c
+++ parser.c
@@ -1,85 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mman <mman@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/26 14:12:31 by mman              #+#    #+#             */
+/*   Updated: 2024/05/26 14:17:59 by mman             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "miniRT.h"
 
 // Function to parse the sphere data
-void parse_sphere_data(char* line, t_scene** scene) {
+void parse_sphere_data(char* line, t_scene** scene)
+{
 	// Check if the line starts with 'sp'
-	if (ft_strncmp(line, "sp", 2) == 0) {
-		// Extract the sphere data from the line
-		// Assuming the format is "sp x y z radius"
-		float x, y, z, radius;
-		sscanf(line, "sp %f %f %f %f", &x, &y, &z, &radius);
-		// Create a new sphere object
-		t_sphere* sphere = malloc(sizeof(t_sphere));
-		sphere->center.x = x;
-		sphere->center.y = y;
-		sphere->center.z = z;
-		sphere->radius = radius;
-		// Add the sphere to the scene's object list
-		(*scene)->objects->raw_data[(*scene)->objects->count] = sphere;
-		(*scene)->total_objects++;
+	if (ft_strncmp(line, "sp", 2) == 0)
+	{
+		ft_pntf("beep boop im a sphere, %i", scene);
 	}
 }
 
 // Function to parse the plane data
-void parse_plane_data(char* line, t_scene** scene) {
+void parse_plane_data(char* line, t_scene** scene)
+{
 	// Check if the line starts with 'pl'
-	if (ft_strncmp(line, "pl", 2) == 0) {
-		// Extract the plane data from the line
-		// Assuming the format is "pl x y z nx ny nz"
-		float x, y, z, nx, ny, nz;
-		sscanf(line, "pl %f %f %f %f %f %f", &x, &y, &z, &nx, &ny, &nz);
-		// Create a new plane object
-		t_plane* plane = malloc(sizeof(t_plane));
-		plane->point.x = x;
-		plane->point.y = y;
-		plane->point.z = z;
-		plane->normal.x = nx;
-		plane->normal.y = ny;
-		plane->normal.z = nz;
-		// Add the plane to the scene's object list
-		(*scene)->objects->raw_data[(*scene)->objects->count] = plane;
-		(*scene)->total_objects++;
+	if (ft_strncmp(line, "pl", 2) == 0)
+	{
+		ft_pntf("beep boop im a plane, %i", scene);
 	}
 }
 
 // Function to parse the cylinder data
 void parse_cylinder_data(char* line, t_scene** scene) {
 	// Check if the line starts with 'cl'
-	if (ft_strncmp(line, "cl", 2) == 0) {
-		// Extract the cylinder data from the line
-		// Assuming the format is "cl x y z nx ny nz diameter height"
-		float x, y, z, nx, ny, nz, diameter, height;
-		sscanf(line, "cl %f %f %f %f %f %f %f %f", &x, &y, &z, &nx, &ny, &nz, &diameter, &height);
-		// Create a new cylinder object
-		t_cylinder* cylinder = malloc(sizeof(t_cylinder));
-		cylinder->center.x = x;
-		cylinder->center.y = y;
-		cylinder->center.z = z;
-		cylinder->normal.x = nx;
-		cylinder->normal.y = ny;
-		cylinder->normal.z = nz;
-		cylinder->diameter = diameter;
-		cylinder->height = height;
-		// Add the cylinder to the scene's object list
-		(*scene)->objects->raw_data[(*scene)->objects->count] = cylinder;
-		(*scene)->objects->count++;
+	if (ft_strncmp(line, "cl", 2) == 0)
+	{
+		ft_pntf("beep boop im a cylinder, %i", scene);
 	}
 }
 
 void parse_camera_data(char* line, t_scene** scene)
 {
 	char	**split;
-	t_vec	*coords;
-	t_vec	*orientation;
-
-	split = NULL;
-	coords = NULL;
-	orientation = NULL;
+	// t_vec	*coords;
+	// t_vec	*orientation;
+	// coords = NULL;
+	// orientation = NULL;
 	if (ft_strncmp(line, "C", 1) == 0) 
 	{	
+		ft_pntf("beep boop im a cylinder, %i", scene);
 		split = ft_split(line, ' ');
 	}
+	else
+		split = NULL;
+	ft_pntf("beep boop im a cylinder, %s", split);
 }
 
 // Function to parse the input file
--- vector_allocation.c
+++ vector_allocation.c
@@ -5,10 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mman <mman@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/18 17:48:16 by mman              #+#    #+#             */
-/*   Updated: 2024/05/18 17:49:11 by mman             ###   ########.fr       */
+/*   Created: 2024/05/26 14:10:04 by mman              #+#    #+#             */
+/*   Updated: 2024/05/26 14:14:18 by mman             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
+//takes in the vector structure, assigns values from string
+//string is converted into a double and assigned to the vector
+//string is in the format "x,y,z"
+void ft_assign_values_to_t_vec(t_vec *vec, char *str)
+{
+    char    **token;
+    char    *copy = ft_strdup(str); // Make a copy of the string to avoid modifying the original
+
+    // Parse the string using strtok
+    token = ft_split(copy, ',');
+    vec->x = ft_atoidouble(token[0]);
+    vec->y = ft_atoidouble(token[1]);
+    vec->z = ft_atoidouble(token[2]);
+    // Free the memory allocated by ft_split
+    free(copy);
+    free(token);
+}
